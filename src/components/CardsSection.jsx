25c99fcca979d99c20708d5756d5d154a564dfab
Benji <Benjamin.lee.2021@gmail.com>
2023-01-27 21:41:29 +1030

    :bug: Fixed issue with icon naming

--- src/components/CardsSection.jsx
+++ src/components/CardsSection.jsx
@@ -20,25 +20,25 @@ export default function CardsSection() {
       />
       <Card
         title="Design"
-        image="/icon-Design.svg"
+        image="/icon-design.svg"
         description="Create beautiful, usable interfaces to help shape the future of how
         the web looks."
       />
       <Card
         title="Photography"
-        image="/icon-Photography.svg"
+        image="/icon-photography.svg"
         description="Explore critical fundamentals like lighting, composition, and focus to
         capture exceptional photos."
       />
       <Card
         title="Crypto"
-        image="/icon-Crypto.svg"
+        image="/icon-crypto.svg"
         description="All you need to know to get started investing in crypto. Go from
         beginner to advanced with this 54 hour course."
       />
       <Card
         title="Business"
-        image="/icon-Business.svg"
+        image="/icon-business.svg"
         description="A step-by-step playbook to help you start, scale, and sustain your
         business without outside investment."
       />
